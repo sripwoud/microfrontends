1faf012c6339737f60e2a4a06f7137331e030f44
sripwoud <me@sripwoud.xyz>
2020-12-19 13:57:24 +0100

    fix remote URL

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
       },
       shared
     })
