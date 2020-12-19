3b66e66ffca7d082fabc3388f163c46a646c846d
sripwoud <me@sripwoud.xyz>
2020-12-19 13:58:24 +0100

    Add publicPath config for marketing package

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,10 @@ const commonConfig = require('./webpack.common')
 
 const prodConfig = {
   mode: 'production',
-  output: { filename: '[name].[contenthash].js' },
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/'
+  },
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
